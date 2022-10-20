e4afb8881894d6ec42fd7c89f2ebf8d5466ba6f1
HenriqueSerres <henriqueserres@gmail.com>
2022-10-20 16:50:39 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file):           
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, "r") as arquivo: 
+            content = arquivo.read()
+            return content.splitlines()
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+
+# video de como imprimor stderr => https://www.youtube.com/watch?v=ewCghx53Dgg
+# como separar \n => https://www.w3schools.com/python/ref_string_splitlines.asp
