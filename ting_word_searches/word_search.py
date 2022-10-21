73a84bbf9d337618d113a58a2a53637ade0cb33d
HenriqueSerres <henriqueserres@gmail.com>
2022-10-21 15:54:48 -0300

    req 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -15,4 +15,16 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_occurrences = []
+    the_word = word.upper()
+    for content in instance._data:
+        for index, line in enumerate(content["linhas_do_arquivo"]):
+            if the_word in line.upper():
+                word_occurrences.append({"linha": index + 1, "conteudo": line})
+    if word_occurrences:
+        return [{
+            "palavra": word,
+            "arquivo": content["nome_do_arquivo"],
+            "ocorrencias": word_occurrences,
+        }]
+    return word_occurrences
